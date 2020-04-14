414c663dd534133148b9543fd404f1b0214bbe5f
maoru <maorutian@gmail.com>
2020-04-13 20:09:48 -0400

    Updates

---
+++ precache-manifest.68c2316774d001e1bf40e71754ca94fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4e0b1f2034dadb8bceebaf0b62c638",
+    "url": "/cake-factory-client/index.html"
+  },
+  {
+    "revision": "9bfdd31b8469492574f4",
+    "url": "/cake-factory-client/static/css/2.be936caf.chunk.css"
+  },
+  {
+    "revision": "51de52628494bcfd7392",
+    "url": "/cake-factory-client/static/css/main.6e5ff2d6.chunk.css"
+  },
+  {
+    "revision": "9bfdd31b8469492574f4",
+    "url": "/cake-factory-client/static/js/2.279fe6f7.chunk.js"
+  },
+  {
+    "revision": "51de52628494bcfd7392",
+    "url": "/cake-factory-client/static/js/main.616896b6.chunk.js"
+  },
+  {
+    "revision": "e67403559e4d36a2d2d6",
+    "url": "/cake-factory-client/static/js/runtime-main.7a950995.js"
+  },
+  {
+    "revision": "db15b81aff768c1b8024d198b6190ddd",
+    "url": "/cake-factory-client/static/media/bg.db15b81a.jpg"
+  },
+  {
+    "revision": "09f15b0cb41d6808e59cd7ab0d3e3cf8",
+    "url": "/cake-factory-client/static/media/logo.09f15b0c.png"
+  }
+]);
